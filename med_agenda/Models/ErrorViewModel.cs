d2af5d6973a912c6d3f85d21dcf2d143a076ab29
John <johnmaravelis@gmail.com>
2022-01-22 16:14:57 +0200

    fixed build bug to project and changed the .net core to 5.0

---
+++ med_agenda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace med_agenda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
